a09c6c441e17359580cf6b1cecc7db738b31032a
BrandonFields <brandonefields@gmail.com>
2021-08-21 13:47:56 -0600

    auth/login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, "yerrr")
+            render json: {user: @user, token: @token}, status: :accepted
+        else
+            render json: {errors: [ message: 'Invalid username or password']}, status: :unauthorized
+        end
+    end
+
+    
 end
